ba821fb379c1824009953859b9793d07c2f66dca
MaksBarabanshikov <maksimbarabanshikov2001@gmail.com>
2022-11-17 00:07:50 +0400

    [17.11.22]

--- contracts/migrations/2_deploy_contracts.js
+++ contracts/migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
 const Treasurexpress = artifacts.require("./Treasurexpress.sol");
 
+const currentWallet = "0xf7C25993801C0BaA63360C1774B117C82F14FdA7"
+
+const testWallet = "0xcF9bbF734430b36f284B51f535ba12371C18575e"
+
 module.exports = function(deployer) {
-    deployer.deploy(Treasurexpress, "0xf7C25993801C0BaA63360C1774B117C82F14FdA7");
+    deployer.deploy(Treasurexpress, testWallet);
 };
