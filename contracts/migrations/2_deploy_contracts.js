ab3c332ec93ba47d16efe3edcebb82dda17a6c13
Zhlobochka <Varyazhloba@yandex.ru>
2022-12-24 14:02:38 +0400

    refactor v1

--- contracts/migrations/2_deploy_contracts.js
+++ contracts/migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ const Treasurexpress = artifacts.require("./Treasurexpress.sol");
 
 const currentWallet = "0x60c55eAc3dA366e3807725D9a551345918d18De0"
 
-const testWallet = "0xEdC9b3311cc153d1409a37Bf53F54e1E247716aD"
+const testWallet = "0x7f4312DDb90301Ef4b2d59868e94643b6a43734e"
 
 module.exports = function(deployer) {
     deployer.deploy(Treasurexpress, testWallet);
